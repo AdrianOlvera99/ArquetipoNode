70eb1a71089d59d2dc295b26baf7bbbcf5324103
AdrianOlvera99 <adrian.olvera@axity.com>
2024-04-05 21:01:29 -0600

    Se añade practica de Node 2

---
+++ user-service/src/services/User/interface.ts
@@ -0,0 +1,27 @@
+
+import User from "../../models/User.model";
+import { UserTo } from "../../to/UserTo";
+
+/**
+ * @export
+ * @interface IUserService
+ */
+export interface IUserService {
+
+    /**
+     * @returns {Promise<any[]>}
+     * @memberof IUserService
+     */
+    findAll(): Promise<any[]>;
+
+    create(user: UserTo): Promise<void>;
+
+    validate(user: UserTo): Promise<boolean>;
+
+    update(id: number, userTO: UserTo): Promise<[number, User[]]>;
+
+    deleteUser(id: number): Promise<number>;
+
+    validateDelete(user: any): Promise<void>;
+
+}
